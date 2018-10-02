c8ee2b8984c98b0b43c8a8a370351adfa9f3d1f7
Carlos Rosas <fantaso.code@gmail.com>
2018-10-02 17:35:12 +0200

    bp_shop: 1. created the blueprint template with all templates available

---
+++ fantaso/bp_shop/views.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+
+
+shop = Blueprint(
+    'shop',
+    __name__,
+    template_folder='templates',
+    # static_folder='static',
+)
+
+
+##################
+# index
+##################
+@shop.route('/', methods=['GET'])
+def index():
+    return render_template('bp_shop/index.html')
+
+##################
+# search
+##################
+@shop.route('/search', methods=['GET'])
+def search():
+    return render_template('bp_shop/search.html')
+
+##################
+# product
+##################
+@shop.route('/product', methods=['GET'])
+def product():
+    return render_template('bp_shop/product.html')
+
+##################
+# cart
+##################
+@shop.route('/cart', methods=['GET'])
+def cart():
+    return render_template('bp_shop/cart.html')
+
+##################
+# chechout
+##################
+@shop.route('/chechout', methods=['GET'])
+def chechout():
+    return render_template('bp_shop/chechout.html')
